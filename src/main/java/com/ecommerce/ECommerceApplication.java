3daddb2c7db962e9600b39c4e588b9edd5f292fa
Gargi P <gargi@batch12.local>
2023-09-30 14:08:35 +0530

    Gargi

--- src/main/java/com/ecommerce/ECommerceApplication.java
+++ src/main/java/com/ecommerce/ECommerceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ECommerceApplication {
 
 	// soumya 
-	// Gargi
+	// Gargi1
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
