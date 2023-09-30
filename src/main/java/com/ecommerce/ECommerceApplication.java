39a5ccde5884e7b64e4c4665dc3763a4a8d96823
unknown <Priyanka@Station16.Priyanka.org>
2023-09-30 14:24:46 +0530

    priyanka commits

--- src/main/java/com/ecommerce/ECommerceApplication.java
+++ src/main/java/com/ecommerce/ECommerceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ECommerceApplication {
 
 	// soumya 
+	//priyanka
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
